fb3a7b5e4707e6ad0dcafec8c242a6c226bd0511
A B M Asadullah <abmasadullah@gmail.com>
2021-01-08 23:00:14 +0600

    Add question model with controller and view by scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_31_145716) do
+ActiveRecord::Schema.define(version: 2021_01_08_092219) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2020_12_31_145716) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_12_31_145716) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "questions", "users"
 end
